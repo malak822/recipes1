9746e71c38d212a9e8ea482018c7f637507aadf9
Malak <malak.serinee@gmail.com>
2026-05-30 21:53:31 +0100

    add APP_URL

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Parse DATABASE_URL
 export DB_CONNECTION=pgsql
 export DB_HOST=dpg-d8djv8navr4c73frmhug-a
 export DB_PORT=5432
 export DB_DATABASE=recipes_db_4k50
 export DB_USERNAME=recipes_db_4k50_user
 export DB_PASSWORD=tL1uyJI6Mz4NTE2EPvtnRD9ZSGyxyLiu
+export APP_URL=https://recipes1-4es9.onrender.com
 php artisan config:clear
 php artisan migrate --force
 apache2-foreground
