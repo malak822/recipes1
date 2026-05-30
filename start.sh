0ae8202f46edc6c55719416e61f1ee68e75c9cf9
Malak <malak.serinee@gmail.com>
2026-05-30 22:11:35 +0100

    auto import recipes on deploy

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ export DB_USERNAME=recipes_db_4k50_user
 export DB_PASSWORD=tL1uyJI6Mz4NTE2EPvtnRD9ZSGyxyLiu
 export APP_URL=https://recipes1-4es9.onrender.com
 export SESSION_SECURE_COOKIE=false
-export ASSET_URL=https://recipes1-4es9.onrender.com
 php artisan config:clear
 php artisan migrate --force
+php artisan import:recipes
 apache2-foreground
